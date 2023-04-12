aebd232027c7ee74ccad7351361419d66c28f413
ahmadrpasha <92101635+ahmadrpasha@users.noreply.github.com>
2023-04-12 00:06:32 -0400

    Adding more practice files

---
+++ Running_Node_Script/boilerplate.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const folderName = process.argv[2] || 'Project';
+
+// fs.mkdir('Dog', { recursive: true }, (err) => {
+//     console.log('In the callback!');
+//     if (err) throw err;
+// });
+try {
+    fs.mkdirSync(folderName);
+    fs.writeFileSync(`${folderName}/index.html`, '');
+    fs.writeFileSync(`${folderName}/styles.css`, '');
+    fs.writeFileSync(`${folderName}/app.js`, '');
+} catch (e) {
+    console.log('Something went wrong!');
+    console.log(e);
+
+}
